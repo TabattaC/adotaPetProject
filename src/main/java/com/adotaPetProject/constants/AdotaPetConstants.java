6bf4d74ee402a601b200ae8431e801684f2aee06
TabattaC <tabattacerri17@gmail.com>
2024-11-10 23:05:23 -0300

    Add AdotaPetConstants class for common static messages
    Created a new AdotaPetConstants class to centralize common static message strings used in the project. This includes messages for errors, invalid data, existing email notifications, and registration success.

---
+++ src/main/java/com/adotaPetProject/constants/AdotaPetConstants.java
@@ -0,0 +1,9 @@
+package com.adotaPetProject.constants;
+
+public class AdotaPetConstants {
+    public static final String SOMETHING_WENT_WRONG = "Somehtin Went Wrong.";
+    public static final String INVALID_DATA = "Invalid Data.";
+    public static final String EMAIL_ALREADY_EXIST = "Email already exist";
+    public static final String SUCCESSFULLY_REGISTERED = "Successfully Registered.";
+
+}
