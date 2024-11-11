c5cd2d5716982e3a4370b96eeae797b76218d0d8
TabattaC <tabattacerri17@gmail.com>
2024-11-10 23:06:33 -0300

    Add UserRestImplement controller for user sign-up functionality
    Introduced UserRestImplement class to handle user sign-up requests. Integrated UserService and exception handling for robust user registration. This change facilitates a structured approach to user-related REST operations.

---
+++ src/main/java/com/adotaPetProject/restImplement/UserRestImplement.java
@@ -0,0 +1,26 @@
+package com.adotaPetProject.restImplement;
+
+import com.adotaPetProject.constants.AdotaPetConstants;
+import com.adotaPetProject.handler.BusinessException;
+import com.adotaPetProject.rest.UserRest;
+import com.adotaPetProject.service.UserService;
+import com.adotaPetProject.utils.AdotaPetUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
+@Controller
+public class UserRestImplement implements UserRest {
+    @Autowired
+    UserService userService;
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        try{
+        return userService.signup(requestMap);
+        }catch (Exception ex){
+            throw new BusinessException(AdotaPetConstants.SOMETHING_WENT_WRONG);
+        }
+    }
+}
