346dadbcf99c1a3d6d6e5c87be8b3f061c13a431
TabattaC <tabattacerri17@gmail.com>
2024-11-18 23:16:08 -0300

    add deal with name exceptions

--- src/main/java/com/adotaPetProject/handler/GlobalExceptionHandler.java
+++ src/main/java/com/adotaPetProject/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.adotaPetProject.handler;
 
 import jakarta.annotation.Resource;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,18 +14,20 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Locale;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Resource
     private MessageSource messageSource;
+
     private HttpHeaders headers() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return headers;
     }
 
-    private ResponseError responseError(String message, HttpStatus statusCode){
+    private ResponseError responseError(String message, HttpStatus statusCode) {
         ResponseError responseError = new ResponseError();
         responseError.setStatus("error");
         responseError.setError(message);
@@ -33,19 +36,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    private ResponseEntity<Object> handleGeneral(Exception e, WebRequest request){
-        if(e.getClass().isAssignableFrom(UndeclaredThrowableException.class)){
-            UndeclaredThrowableException exception =(UndeclaredThrowableException) e;
+    private ResponseEntity<Object> handleGeneral(Exception e, WebRequest request) {
+        String message;
+        if (e.getClass().isAssignableFrom(UndeclaredThrowableException.class)) {
+            UndeclaredThrowableException exception = (UndeclaredThrowableException) e;
             return handleBusinessException((BusinessException) exception.getUndeclaredThrowable(), request);
-        }else{
-            String message = messageSource.getMessage("error.server", new Object[]{e.getMessage()}, null);
-            ResponseError error = responseError(message,HttpStatus.INTERNAL_SERVER_ERROR);
-            return handleExceptionInternal(e, error, headers(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        } else {
+            Locale locale = Locale.getDefault();
+            try {
+                message = messageSource.getMessage("error.server", new Object[]{e.getMessage()}, locale);
+            } catch (NoSuchMessageException ex) {
+                // Mensagem padrão em caso de ausência no arquivo
+                message = "Erro no servidor: " + e.getMessage();
+            }
         }
+        ResponseError error = responseError(message, HttpStatus.INTERNAL_SERVER_ERROR);
+        return handleExceptionInternal(e, error, headers(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
+
     @ExceptionHandler({BusinessException.class})
-    private ResponseEntity<Object> handleBusinessException(BusinessException e, WebRequest request){
-        ResponseError error = responseError(e.getMessage(),HttpStatus.CONFLICT);
+    private ResponseEntity<Object> handleBusinessException(BusinessException e, WebRequest request) {
+        ResponseError error = responseError(e.getMessage(), HttpStatus.CONFLICT);
         return handleExceptionInternal(e, error, headers(), HttpStatus.CONFLICT, request);
     }
 
