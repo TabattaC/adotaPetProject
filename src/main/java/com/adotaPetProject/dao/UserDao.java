03063dd3019025fe917c7bca61f229ba59adec90
TabattaC <tabattacerri17@gmail.com>
2024-11-20 15:55:01 -0300

    add updateStatus query

--- src/main/java/com/adotaPetProject/dao/UserDao.java
+++ src/main/java/com/adotaPetProject/dao/UserDao.java
@@ -2,13 +2,21 @@ package com.adotaPetProject.dao;
 
 import com.adotaPetProject.model.User;
 import com.adotaPetProject.wrapper.UserWrapper;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Map;
 
-public interface UserDao extends JpaRepository<User,Integer> {
+public interface UserDao extends JpaRepository<User, Integer> {
 
-    User findByEmailId(@Param("email")String email);
+    User findByEmailId(@Param("email") String email);
+
     List<UserWrapper> getAllUsers();
+
+    @Modifying
+    @Transactional
+    Integer updateStatus(@Param("status") String status, @Param("id") Integer id);
 }
