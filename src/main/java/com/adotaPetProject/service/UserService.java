bf69bc80ac9a2c350384af2a275b97acc4eea1e5
TabattaC <tabattacerri17@gmail.com>
2024-11-20 11:48:50 -0300

    add service getALlUsers

--- src/main/java/com/adotaPetProject/service/UserService.java
+++ src/main/java/com/adotaPetProject/service/UserService.java
@@ -1,11 +1,15 @@
 package com.adotaPetProject.service;
 
+import com.adotaPetProject.wrapper.UserWrapper;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserService {
     ResponseEntity<String> signup(Map<String,String> requestMap);
 
     ResponseEntity<String> login(Map<String,String> requestMap);
+
+    ResponseEntity<List<UserWrapper>> getAllUsers();
 }
