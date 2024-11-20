98cc05a8c08f0cad45ab4da42f3a61271d599437
TabattaC <tabattacerri17@gmail.com>
2024-11-20 15:35:41 -0300

    add  List<UserWrapper> getAllUsers();

--- src/main/java/com/adotaPetProject/dao/UserDao.java
+++ src/main/java/com/adotaPetProject/dao/UserDao.java
@@ -1,10 +1,14 @@
 package com.adotaPetProject.dao;
 
 import com.adotaPetProject.model.User;
+import com.adotaPetProject.wrapper.UserWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserDao extends JpaRepository<User,Integer> {
 
     User findByEmailId(@Param("email")String email);
+    List<UserWrapper> getAllUsers();
 }
