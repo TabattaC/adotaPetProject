6fe162b71a56a05c8fff8e5e8d887d8864872ccf
TabattaC <tabattacerri17@gmail.com>
2024-11-20 15:54:19 -0300

    add texts

--- src/main/java/com/adotaPetProject/constants/AdotaPetConstants.java
+++ src/main/java/com/adotaPetProject/constants/AdotaPetConstants.java
@@ -5,5 +5,7 @@ public class AdotaPetConstants {
     public static final String INVALID_DATA = "Invalid Data.";
     public static final String EMAIL_ALREADY_EXIST = "Email already exist";
     public static final String SUCCESSFULLY_REGISTERED = "Successfully Registered.";
+    public static final String UNAUTHORIZED_ACESS = "Unauthorized Acess.";
+    public static final String USER_DOESNOT_EXIST = "User ID does not exist.. ";
 
 }
