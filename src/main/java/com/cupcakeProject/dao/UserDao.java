32be33a7fd3a58e4274de2d52d23eedf18dcce50
TabattaC <tabattacerri17@gmail.com>
2024-11-21 22:01:20 -0300

    add new mehtod getAllAdmin

--- src/main/java/com/cupcakeProject/dao/UserDao.java
+++ src/main/java/com/cupcakeProject/dao/UserDao.java
@@ -1,14 +1,13 @@
-package com.adotaPetProject.dao;
+package com.cupcakeProject.dao;
 
-import com.adotaPetProject.model.User;
-import com.adotaPetProject.wrapper.UserWrapper;
+import com.cupcakeProject.model.User;
+import com.cupcakeProject.wrapper.UserWrapper;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Map;
 
 public interface UserDao extends JpaRepository<User, Integer> {
 
@@ -16,7 +15,10 @@ public interface UserDao extends JpaRepository<User, Integer> {
 
     List<UserWrapper> getAllUsers();
 
+    List<String> getAllAdmin();
+
     @Modifying
     @Transactional
     Integer updateStatus(@Param("status") String status, @Param("id") Integer id);
+
 }
