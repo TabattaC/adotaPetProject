ff313c76abb218f5533f2b0f7cf6b470961b7de0
TabattaC <tabattacerri17@gmail.com>
2024-11-21 23:08:38 -0300

    add new constants

--- src/main/java/com/cupcakeProject/constants/AdotaPetConstants.java
+++ src/main/java/com/cupcakeProject/constants/AdotaPetConstants.java
@@ -7,6 +7,9 @@ public class AdotaPetConstants {
     public static final String SUCCESSFULLY_REGISTERED = "Successfully Registered.";
     public static final String UNAUTHORIZED_ACESS = "Unauthorized Acess.";
     public static final String USER_DOESNOT_EXIST = "User ID does not exist.. ";
-    public static final String UPDATE_SUCESS = "User Status Updated Sucessfully ";
+    public static final String UPDATE_SUCESS = "User Status Updated Sucessfully.";
+    public static final String INCORRECT_OLD_PASSWORD = "Incorrect Old Password.";
+    public static final String PASSWORD_UPDATED = "Password Updated Successfully.";
+    public static final String CHECK_EMAIL = "Check your mail for Credentials.";
 
 }
