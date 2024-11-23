a60a221b83f05f2f1274c3ba74d9f018298c2b01
TabattaC <tabattacerri17@gmail.com>
2024-11-23 17:56:18 -0300

    add new querys for updateProductStatus,getProductByCategory, getproductById

--- src/main/java/com/cupcakeProject/model/Product.java
+++ src/main/java/com/cupcakeProject/model/Product.java
@@ -6,7 +6,10 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.io.Serializable;
-@NamedQuery(name = "Product.getAllProduct", query = "select new com.cupcakeProject.wrapper.ProductWrapper(p.id,  p.name, p.description, p.price, p.status, p.category.id, p.category.name ) from Product p")
+@NamedQuery(name = "Product.getAllProduct", query = "select new com.cupcakeProject.wrapper.ProductWrapper(p.id, p.name, p.description, p.price, p.status, p.category.id, p.category.name) from Product p")
+@NamedQuery(name = "Product.updateProductStatus", query = "update Product p set p.status=status where p.id=:id ")
+@NamedQuery(name = "Product.getProductByCategory", query = "select new com.cupcakeProject.wrapper.ProductWrapper(p.id, p.name) from Product p where p.category.id = :id and p.status='true'")
+@NamedQuery(name = "Product.getproductById", query = "select new com.cupcakeProject.wrapper.ProductWrapper(p.id, p.name, p.description, p.price) from Product p where p.id = :id")
 
 
 @Data
