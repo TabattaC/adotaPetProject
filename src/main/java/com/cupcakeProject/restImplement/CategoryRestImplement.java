2c7e7d609267c52820e67a2bd9b143c8edee872a
TabattaC <tabattacerri17@gmail.com>
2024-11-22 23:00:37 -0300

    add category rest interface add addNewCategory method to add a new category add getAllCategory to gett all category save add updateCategory to update a category

---
+++ src/main/java/com/cupcakeProject/restImplement/CategoryRestImplement.java
@@ -0,0 +1,53 @@
+package com.cupcakeProject.restImplement;
+
+import com.cupcakeProject.model.Category;
+import com.cupcakeProject.rest.CategoryRest;
+import com.cupcakeProject.service.CategoryService;
+import com.cupcakeProject.utils.CupcakeProjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.cupcakeProject.constants.AdotaPetConstants.SOMETHING_WENT_WRONG;
+
+@Controller
+public class CategoryRestImplement implements CategoryRest {
+    @Autowired
+    CategoryService categoryService;
+
+    @Override
+    public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
+        try {
+            return categoryService.addNewCategory(requestMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return CupcakeProjectUtils.getResponseEntity(SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try {
+            return categoryService.getAllCategory(filterValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+       try{
+           return categoryService.updateCategory(requestMap);
+       }catch (Exception e){
+
+       }
+        return CupcakeProjectUtils.getResponseEntity(SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
