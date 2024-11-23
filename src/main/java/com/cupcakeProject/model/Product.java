f380064ac657b7fe2592048ebd723c590fa0abd3
TabattaC <tabattacerri17@gmail.com>
2024-11-23 13:08:24 -0300

    add new model for product with tags @Data                                     @Entity                                     @DynamicUpdate                                     @DynamicInsert

---
+++ src/main/java/com/cupcakeProject/model/Product.java
@@ -0,0 +1,40 @@
+package com.cupcakeProject.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "product")
+public class Product implements Serializable {
+
+    public static long serialVersionUId = 123456L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_fk", nullable = false)
+    private Category category;
+
+
+}
