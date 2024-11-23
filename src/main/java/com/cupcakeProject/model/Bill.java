c598b58ab31d9a064ca23decf3ab66d42f9dd1fd
TabattaC <tabattacerri17@gmail.com>
2024-11-23 19:26:11 -0300

    add bill model

---
+++ src/main/java/com/cupcakeProject/model/Bill.java
@@ -0,0 +1,48 @@
+package com.cupcakeProject.model;
+
+import io.swagger.models.auth.In;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "bill")
+public class Bill implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "uuid")
+    private String uuid;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "contactnumber")
+    private String contactNumber;
+
+    @Column(name = "paymentmethod")
+    private String paymentMethod;
+
+    @Column(name = "total")
+    private Integer total;
+
+    @Column(name = "productdetails",columnDefinition = "json")
+    private String productDetails;
+
+    @Column(name = "createdby")
+    private String createdBy;
+}
