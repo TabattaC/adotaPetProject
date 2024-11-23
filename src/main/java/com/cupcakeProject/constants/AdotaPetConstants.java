3b717aacc578011253e768c2aea778978c8e0ab9
TabattaC <tabattacerri17@gmail.com>
2024-11-22 23:02:27 -0300

    add new constants

--- src/main/java/com/cupcakeProject/constants/AdotaPetConstants.java
+++ src/main/java/com/cupcakeProject/constants/AdotaPetConstants.java
@@ -1,7 +1,7 @@
 package com.cupcakeProject.constants;
 
 public class AdotaPetConstants {
-    public static final String SOMETHING_WENT_WRONG = "Somehtin Went Wrong.";
+    public static final String SOMETHING_WENT_WRONG = "Something went wrong.";
     public static final String INVALID_DATA = "Invalid Data.";
     public static final String EMAIL_ALREADY_EXIST = "Email already exist";
     public static final String SUCCESSFULLY_REGISTERED = "Successfully Registered.";
@@ -11,5 +11,6 @@ public class AdotaPetConstants {
     public static final String INCORRECT_OLD_PASSWORD = "Incorrect Old Password.";
     public static final String PASSWORD_UPDATED = "Password Updated Successfully.";
     public static final String CHECK_EMAIL = "Check your mail for Credentials.";
+    public static final String CATEGORY_ADD = "Category add successfully.";
 
 }
